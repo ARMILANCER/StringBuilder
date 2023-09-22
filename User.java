87193f568f3695ccf1505c95dd5c46d583aec4b7
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2023-09-22 10:02:47 +0200

    Update User.java

--- User.java
+++ User.java
@@ -2,20 +2,23 @@ package User;
 import java.io.IOException;
 import java.util.Calendar;
 public class User {
+	// maximum length for name.
 	private final static int DIM_MAX_NAME = 20;
+	// maximum size of the record composed of two strings and an integer.
 	private final static int DIM_RECORD = 2 *(DIM_MAX_NAME+Character.BYTES)+Integer.BYTES;
+	// set of two strings and an integer, maximum length "DIM_RECORD"
 	private StringBuilder buffer = new StringBuilder();
 	public boolean addName(String name) {
 		if(name.length() <= DIM_RECORD) {
 			buffer.append(name);
 			for(int i=0;i<(DIM_MAX_NAME-name.length());i++)
 				buffer.append('\0');
-		}else return false;    
+		}else return false; 
 		return true;
 	}
 	public boolean addYOB(String YOB) {
 		if(Integer.valueOf(YOB) <= Calendar.getInstance().get(Calendar.YEAR)) {
-			buffer.append(String.valueOf(YOB));
+			buffer.append(YOB);
 			for(int i=0;i<(DIM_MAX_NAME-YOB.length());i++)
 				buffer.append('\0');
 		}else return false;
@@ -23,6 +26,5 @@ public class User {
 	}
 	public String getInfo() {
 		return buffer.toString();
-		//return buffer.substring(DIM_MAX_NAME);
 	}
 }
